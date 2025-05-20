a6043f1c5022c79eeb2faa4214b6ef72dfe6e2a1
david berrio <david.berrio@upb.edu.co>
2025-05-20 14:31:25 -0500

    Tu mensaje de commit

---
+++ Desktop/BibliotecaFarmacia/Clases/ValidacionesGenerales.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFarmacia.Clases
+{
+    internal class ValidacionesGenerales
+    {
+
+        string mensaje_vacio = "Este campo no puede estar vacío \n";
+
+        public string Mensaje_vacio { get => mensaje_vacio; set => mensaje_vacio = value; }
+
+        public ValidacionesGenerales()
+        {
+            
+        }
+
+        internal bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ Desktop/BibliotecaFarmacia/Eventos/Publisher_P_Acumulados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BibliotecaFarmacia.Clases
+{
+    public delegate void delegado_ptos_compra(string ptos);
+
+    public class Publisher_P_Acumulados
+    {
+        public event delegado_ptos_compra evento_ptos;
+
+        public void Informar_P_Acumulados(string ptos)
+        {
+            if (evento_ptos != null)
+            {
+                evento_ptos(ptos);
+            }
+        }
+    }
+}
+
+
