319f0f18b7975ec8c925516e446a3ab6f1c29199
Camilo Ardila <cardila55555@gmail.com>
2025-05-19 19:49:43 -0500

    Elaboración base de la clase Persona y su hijo, Cliente

---
+++ BibliotecaFarmacia/BibliotecaFarmacia/Clases/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BibliotecaFarmacia.Clases
+{
+    public class Cliente : Persona
+    {
+        private uint total_gastado;
+        private string ptos;
+
+        public uint Total_gastado
+        {
+            get => total_gastado;
+            set => total_gastado = value;
+        }
+
+        public string Ptos
+        {
+            get => ptos;
+            set => ptos = value ?? "0";
+        }
+
+        public Cliente(string nombre_persona, string cc, string telefono_persona, uint total_gastado, string ptos)
+            : base(nombre_persona, cc, telefono_persona)
+        {
+            Total_gastado = total_gastado;
+            Ptos = ptos;
+        }
+    }
+}
--- BibliotecaFarmacia/BibliotecaFarmacia/Clases/Persona.cs
+++ BibliotecaFarmacia/BibliotecaFarmacia/Clases/Persona.cs
@@ -1,17 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BibliotecaFarmacia.Clases
 {
-    class Persona
+    public class Persona
     {
+        private string nombre_persona = "";
+        private string cc = "";
+        private string telefono_persona = "";
+        private ValidacionesGenerales validaciones = new ValidacionesGenerales();
+        private byte min_long = 7, max_long = 10;
 
+        public string Nombre_persona
+        {
+            get => nombre_persona;
+            set => nombre_persona = value == null ? throw new Exception("El nombre de la persona no puede estar vacío \n") : value;
+        }
 
+        public string Telefono_persona
+        {
+            get => telefono_persona;
+            set
+            {
+                if (value == null)
+                    throw new Exception(validaciones.Mensaje_vacio);
 
+                if (value.Length < min_long || value.Length > max_long)
+                    throw new Exception("El teléfono debe tener entre 7 y 10 números \n");
 
+                if (!validaciones.IsDigitsOnly(value))
+                    throw new Exception("El número de teléfono solo puede contener dígitos del 0 al 9 \n");
 
+                telefono_persona = value;
+            }
+        }
+
+        public string CC
+        {
+            get => cc;
+            set
+            {
+                if (value == null)
+                    throw new Exception(validaciones.Mensaje_vacio);
+
+                if (value.Length < min_long || value.Length > max_long)
+                    throw new Exception("La cédula debe tener entre 7 y 10 números \n");
+
+                if (!validaciones.IsDigitsOnly(value))
+                    throw new Exception("La cédula solo puede contener dígitos del 0 al 9 \n");
+
+                cc = value;
+            }
+        }
+
+        public Persona(string nombre_persona, string cc, string telefono_persona)
+        {
+            Nombre_persona = nombre_persona;
+            CC = cc;
+            Telefono_persona = telefono_persona;
+        }
     }
 }
